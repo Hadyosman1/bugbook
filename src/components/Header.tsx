d77bd768426dc1ede55aee67a13fd37e947dbcce
hady <osmanhady73@gmail.com>
2025-02-04 20:40:50 +0200

    Add starting code and installing packages for development

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="py-3 shadow">
+      <div className="container">Header</div>
+    </header>
+  );
+};
+
+export default Header;
