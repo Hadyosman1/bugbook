0c06eb67f333b675310dd574adea1d929fe51467
hady <osmanhady73@gmail.com>
2025-02-09 22:29:15 +0200

    Add delete post functionality
    - Added DeletePostDialog component for confirming post deletions
    - Updated Post component to include delete post functionality
    - Added PostMoreButton component for additional post actions
    - Created actions file for post-related actions
    - Updated PostEditor component to handle post deletions
    - Modified mutations in post editor to support delete operations
    - Added new mutations file for post-related mutations

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -1,15 +1,23 @@
+"use client";
+
 import { PostData } from "@/lib/types";
 import UserAvatar from "../UserAvatar";
 import Link from "next/link";
 import { formatRelativeDate } from "@/lib/utils";
+import PostMoreButton from "./PostMoreButton";
+import { useSession } from "@/app/(main)/SessionProvider";
 
 interface PostProps {
   post: PostData;
 }
 
-const Post = ({ post: { content, createdAt, id, user } }: PostProps) => {
+const Post = ({ post }: PostProps) => {
+  const { user: currentUser } = useSession();
+  
+  const { content, createdAt, id, user } = post;
+
   return (
-    <article className="space-y-3 rounded-2xl bg-card p-5 shadow-sm">
+    <article className="group space-y-3 rounded-2xl bg-card p-5 shadow-sm">
       <div className="flex flex-wrap items-center gap-3">
         <Link href={`/users/${user.username}`}>
           <UserAvatar
@@ -34,7 +42,12 @@ const Post = ({ post: { content, createdAt, id, user } }: PostProps) => {
           </Link>
         </div>
 
-        {/* <Button variant="secondary">Follow</Button> */}
+        {currentUser.id === user.id && (
+          <PostMoreButton
+            post={post}
+            className="ms-auto self-start transition-opacity duration-300 focus:opacity-100 active:opacity-100 group-hover:opacity-100 md:opacity-0"
+          />
+        )}
       </div>
       <div className="whitespace-pre-line break-words">{content}</div>
     </article>
