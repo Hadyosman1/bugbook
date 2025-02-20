740711db28b646bd12c79e1a58efca9b4b172eb0
hady <osmanhady73@gmail.com>
2025-02-20 16:06:31 +0200

    Enhance post editor and add clear uploads API
    - Add media upload functionality to PostEditor component
    - Added clear-uploads API route to handle clearing uploads
    - Updated uploadthing core functionality in src/app/api/uploadthing/core.ts
    - Enhanced Post component with new features for better user experience
    - Refactored actions in PostEditor for better code organization and maintainability
    - Added useMediaUpload hook to handle media uploads efficiently
    - Updated types in src/lib/types.ts to reflect new data structures
    - Enhanced post validations in src/lib/validations/post.ts for improved data integrity
    - Added vercel.json for deployment configurations (Cron Jobs)

--- src/components/posts/editor/actions.ts
+++ src/components/posts/editor/actions.ts
@@ -5,15 +5,24 @@ import { prisma } from "@/lib/prisma";
 import { getPostDataInclude } from "@/lib/types";
 import { postSchema } from "@/lib/validations/post";
 
-export async function submitPost(input: string) {
+export async function submitPost(input: {
+  content: string;
+  mediaIds: string[];
+}) {
   const { user } = await validateRequest();
 
   if (!user) throw new Error("Unauthorized");
 
-  const { content } = postSchema.parse({ content: input });
+  const { content, mediaIds } = postSchema.parse(input);
 
   const newPost = await prisma.post.create({
-    data: { content, userId: user.id },
+    data: {
+      content,
+      userId: user.id,
+      attachments: {
+        connect: mediaIds.map((id) => ({ id })),
+      },
+    },
     include: getPostDataInclude(user.id),
   });
 
